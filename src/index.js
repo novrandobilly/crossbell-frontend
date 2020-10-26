0ec4f82568634410d8dc97d50d55bcb561f48a2c
crossbell <crossbellcorps@gmail.com>
2020-10-26 13:52:48 +0700

    Create React Redux state management for jobs dummy data & create Job Search Feature

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 import './index.css';
 import App from './App';
-import configureJobsStore from './store/jobs-store';
 import * as serviceWorker from './serviceWorker';
+import jobReducers from './store/job-reducers';
 
-configureJobsStore();
+const store = createStore(jobReducers);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
