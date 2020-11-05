0e80447a047761cf2325c20d1be531ff6565b138
crossbell <crossbellcorps@gmail.com>
2020-11-05 10:05:15 +0700

    Connect Create Company feature to back-end (with dummy auth)

---
+++ src/store/actions/company-actions.js
@@ -0,0 +1,43 @@
+import * as actionTypes from './actions';
+
+const createCompanySuccess = payload => {
+	return {
+		type: actionTypes.CREATECOMPANY,
+		payload: payload
+	};
+};
+const createCompanyFail = () => {
+	return {
+		type: actionTypes.CREATECOMPANYFAIL
+	};
+};
+const createCompanyStart = () => {
+	return {
+		type: actionTypes.CREATECOMPANYSTART
+	};
+};
+
+export const createCompany = companyData => {
+	return async dispatch => {
+		dispatch(createCompanyStart());
+		try {
+			const response = await fetch(`http://localhost:5000/api/users/signup`, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({
+					companyName: companyData.companyName,
+					email: companyData.email,
+					password: companyData.password,
+					isCompany: true
+				})
+			});
+			const responseJSON = await response.json();
+			dispatch(createCompanySuccess(responseJSON.newCompany));
+			return responseJSON.newCompany;
+		} catch (err) {
+			dispatch(createCompanyFail);
+		}
+	};
+};
