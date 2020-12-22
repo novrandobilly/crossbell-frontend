04c16849b8a2130871e4f95658375fd8a1363b2e
crossbell <crossbellcorps@gmail.com>
2020-12-22 15:40:17 +0700

    Reset password feature

--- src/general/pages/Home/Components/Login.js
+++ src/general/pages/Home/Components/Login.js
@@ -1,143 +1,134 @@
-import React from "react";
-import { connect } from "react-redux";
-import { withRouter } from "react-router-dom";
-import * as actionTypes from "../../../../store/actions/actions";
-import * as actionCreators from "../../../../store/actions";
-import { useForm } from "../../../../shared/utils/useForm";
+import React from 'react';
+import { connect } from 'react-redux';
+import { withRouter, Link } from 'react-router-dom';
+import * as actionTypes from '../../../../store/actions/actions';
+import * as actionCreators from '../../../../store/actions';
+import { useForm } from '../../../../shared/utils/useForm';
 
-import Input from "../../../../shared/UI_Element/Input";
-import Spinner from "../../../../shared/UI_Element/Spinner/SpinnerCircle";
-import Modal from "../../../../shared/UI_Element/Modal";
+import Input from '../../../../shared/UI_Element/Input';
+import Spinner from '../../../../shared/UI_Element/Spinner/SpinnerCircle';
+import Modal from '../../../../shared/UI_Element/Modal';
 
-import {
-  VALIDATOR_REQUIRE,
-  VALIDATOR_EMAIL,
-  VALIDATOR_MINLENGTH,
-} from "../../../../shared/utils/validator";
+import { VALIDATOR_REQUIRE, VALIDATOR_EMAIL, VALIDATOR_MINLENGTH } from '../../../../shared/utils/validator';
 
-import classes from "./Login.module.css";
+import classes from './Login.module.css';
 
-const Login = (props) => {
-  const [formState, onInputHandler] = useForm(
-    {
-      email: {
-        value: "",
-        isValid: false,
-      },
-      password: {
-        value: "",
-        isValid: false,
-      },
-    },
-    false
-  );
+const Login = props => {
+	const [ formState, onInputHandler ] = useForm(
+		{
+			email: {
+				value: '',
+				isValid: false
+			},
+			password: {
+				value: '',
+				isValid: false
+			}
+		},
+		false
+	);
 
-  const onSubmitHandler = async (event) => {
-    event.preventDefault();
+	const onSubmitHandler = async event => {
+		event.preventDefault();
 
-    const loginData = {
-      email: formState.inputs.email.value,
-      password: formState.inputs.password.value,
-    };
+		const loginData = {
+			email: formState.inputs.email.value,
+			password: formState.inputs.password.value
+		};
 
-    let res;
-    try {
-      res = await props.loginServer(loginData);
-      if (!res) {
-        throw new Error("Error");
-      }
-    } catch (err) {
-      console.log(err);
-    }
+		let res;
+		try {
+			res = await props.loginServer(loginData);
+			if (!res) {
+				throw new Error('Error');
+			}
+		} catch (err) {
+			console.log(err);
+		}
 
-    if (res.token) {
-      props.history.push("/jobs-dashboard");
-    } else {
-      console.log("error");
-    }
-  };
+		if (res.token) {
+			props.history.push('/jobs-dashboard');
+		} else {
+			console.log('error');
+		}
+	};
 
-  const onCancelHandler = () => {
-    props.logout();
-  };
+	const onCancelHandler = () => {
+		props.logout();
+	};
 
-  let formContent = (
-    <div className={classes.ContainerFlex}>
-      <p className={classes.FormTitle}>Login</p>
+	let formContent = (
+		<div className={classes.ContainerFlex}>
+			<p className={classes.FormTitle}>Login</p>
 
-      <Input
-        inputType="input"
-        id="email"
-        inputClass="Login"
-        validatorMethod={[VALIDATOR_REQUIRE(), VALIDATOR_EMAIL()]}
-        onInputHandler={onInputHandler}
-        label="Email*"
-      />
+			<Input
+				inputType='input'
+				id='email'
+				inputClass='Login'
+				validatorMethod={[ VALIDATOR_REQUIRE(), VALIDATOR_EMAIL() ]}
+				onInputHandler={onInputHandler}
+				label='Email*'
+			/>
 
-      <Input
-        inputType="input"
-        id="password"
-        inputClass="Login"
-        validatorMethod={[VALIDATOR_REQUIRE(), VALIDATOR_MINLENGTH(6)]}
-        onInputHandler={onInputHandler}
-        label="Password*"
-        type="password"
-      />
+			<Input
+				inputType='input'
+				id='password'
+				inputClass='Login'
+				validatorMethod={[ VALIDATOR_REQUIRE(), VALIDATOR_MINLENGTH(6) ]}
+				onInputHandler={onInputHandler}
+				label='Password*'
+				type='password'
+			/>
 
-      <button
-        disabled={!formState.formIsValid}
-        className={classes.SubmitButton}
-      >
-        <span>Submit</span>
-      </button>
+			<button disabled={!formState.formIsValid} className={classes.SubmitButton}>
+				<span>Submit</span>
+			</button>
 
-      <span className={classes.sign}>
-        Don't have an account
-        <button
-          className={classes.ChangeSign}
-          onClick={props.sign}
-          type="button"
-        >
-          Sign Up Here
-        </button>
-      </span>
+			<span className={classes.sign}>
+				Don't have an account
+				<button className={classes.ChangeSign} onClick={props.sign} type='button'>
+					Sign Up Here
+				</button>
+			</span>
 
-      <span className={classes.sign}>
-        Forgot password?
-        <button className={classes.ChangeSign} type="button">
-          Click Here
-        </button>
-      </span>
-    </div>
-  );
+			<span className={classes.sign}>
+				Forgot password?
+				<button className={classes.ChangeSign} type='button'>
+					<Link style={{ textDecoration: 'none', color: 'rgba(58, 81, 153, 1)' }} to='/forgot'>
+						Click Here
+					</Link>
+				</button>
+			</span>
+		</div>
+	);
 
-  if (props.auth.isLoading) {
-    formContent = <Spinner />;
-  }
-  return (
-    <React.Fragment>
-      <form onSubmit={onSubmitHandler} className={classes.Container}>
-        <Modal show={props.auth.isError} onCancel={onCancelHandler}>
-          Email or Password invalid. Please try again.
-        </Modal>
-        {formContent}
-      </form>
-    </React.Fragment>
-  );
+	if (props.auth.isLoading) {
+		formContent = <Spinner />;
+	}
+	return (
+		<React.Fragment>
+			<form onSubmit={onSubmitHandler} className={classes.Container}>
+				<Modal show={props.auth.isError} onCancel={onCancelHandler}>
+					Email or Password invalid. Please try again.
+				</Modal>
+				{formContent}
+			</form>
+		</React.Fragment>
+	);
 };
 
-const mapStateToProps = (state) => {
-  return {
-    auth: state.auth,
-  };
+const mapStateToProps = state => {
+	return {
+		auth: state.auth
+	};
 };
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    isCompany: () => dispatch({ type: actionTypes.AUTHCOMPANY }),
-    logout: () => dispatch({ type: actionTypes.AUTHLOGOUT }),
-    loginServer: (loginData) => dispatch(actionCreators.login(loginData)),
-  };
+const mapDispatchToProps = dispatch => {
+	return {
+		isCompany: () => dispatch({ type: actionTypes.AUTHCOMPANY }),
+		logout: () => dispatch({ type: actionTypes.AUTHLOGOUT }),
+		loginServer: loginData => dispatch(actionCreators.login(loginData))
+	};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(withRouter(Login));
--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,61 +1,40 @@
-export {
-  createCompany,
-  getOneCompany,
-  updateCompanyBriefDescriptions,
-  updateCompanyIntro,
-  updateCompanyPIC,
-} from "./company-actions";
+export { createCompany, getOneCompany, updateCompanyBriefDescriptions, updateCompanyIntro, updateCompanyPIC } from './company-actions';
 
 export {
-  createApplicant,
-  updateApplicantIntro,
-  updateApplicantSummary,
-  updateApplicantEducation,
-  updateApplicantExperience,
-  updateApplicantCertification,
-  updateApplicantSkills,
-  getOneApplicant,
-  deleteSegment,
-} from "./applicant-actions";
+	createApplicant,
+	updateApplicantIntro,
+	updateApplicantSummary,
+	updateApplicantEducation,
+	updateApplicantExperience,
+	updateApplicantCertification,
+	updateApplicantSkills,
+	getOneApplicant,
+	deleteSegment
+} from './applicant-actions';
+
+export { getAllApplicant, getAllJob, admReg, admSignIn, getWholeCompanies, activateCompany, blockCompany } from './admin-actions';
+
+export { createFeed, getFeedback, deleteFeed } from './feedback-actions';
 
 export {
-  getAllApplicant,
-  getAllJob,
-  admReg,
-  admSignIn,
-  getWholeCompanies,
-  activateCompany,
-  blockCompany,
-} from "./admin-actions";
+	createOrder,
+	getOrder,
+	getOrderInvoice,
+	getWholeOrderREG,
+	approveOrder,
+	createOrderES,
+	getWholeOrderES,
+	updateOrderStatusES,
+	getOneOrderES,
+	addCandidateES,
+	updateCandidateStatusES,
+	getCompanyES,
+	getWholeOrderBC,
+	createOrderCandidate,
+	getCompanyBC,
+	deleteCandidateES
+} from './order-actions';
 
-export { createFeed, getFeedback, deleteFeed } from "./feedback-actions";
+export { createJob, getAllAvailableJobs, getOneJob, updateJob, deleteJob, applyJob } from './job-actions';
 
-export {
-  createOrder,
-  getOrder,
-  getOrderInvoice,
-  getWholeOrderREG,
-  approveOrder,
-  createOrderES,
-  getWholeOrderES,
-  updateOrderStatusES,
-  getOneOrderES,
-  addCandidateES,
-  updateCandidateStatusES,
-  getCompanyES,
-  getWholeOrderBC,
-  createOrderCandidate,
-  getCompanyBC,
-  deleteCandidateES,
-} from "./order-actions";
-
-export {
-  createJob,
-  getAllAvailableJobs,
-  getOneJob,
-  updateJob,
-  deleteJob,
-  applyJob,
-} from "./job-actions";
-
-export { login } from "./auth-actions";
+export { login, forgotPwd, getResetPwd, resetPwd } from './auth-actions';
