20ba179ac1d4d1b204b81d76d414af92193a816e
Tommy Pranoto <tommyprn8@gmail.com>
2021-01-12 10:20:34 +0700

    navbar

--- src/shared/Navigation/NavigationLinks.js
+++ src/shared/Navigation/NavigationLinks.js
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink, withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 import * as actionTypes from "../../store/actions/actions";
 
 import SideBar from "./SideBar";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
 
 import classes from "./NavigationLinks.module.css";
 
 const NavigationLinks = (props) => {
+  const [companyDropdown, setCompanyDropdown] = useState(false);
+  const [companyMyList, setCompanyMyList] = useState(false);
+
   const logoutHandler = () => {
     if (props.admin.isLoggedIn) {
       props.admLogout();
@@ -25,6 +29,16 @@ const NavigationLinks = (props) => {
       </li>
     );
   }
+
+  const DropdownOrder = () => {
+    setCompanyMyList(false);
+    setCompanyDropdown(!companyDropdown);
+  };
+
+  const DropdownList = () => {
+    setCompanyMyList(!companyMyList);
+    setCompanyDropdown(false);
+  };
 
   return (
     <div className={classes.NavContainer}>
@@ -50,18 +64,91 @@ const NavigationLinks = (props) => {
           </li>
         )}
         {props.auth.isLoggedIn && props.auth.isCompany && (
-          <li>
-            <NavLink
-              to={`/co/${props.auth.userId}`}
-              activeClassName={classes.active}
-            >
-              My Company Profile
-            </NavLink>
-          </li>
+          <>
+            <li>
+              <NavLink
+                to={`/co/${props.auth.userId}`}
+                activeClassName={classes.active}
+              >
+                My Company Profile
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to={`/jobs/new`} activeClassName={classes.active}>
+                Post New Job
+              </NavLink>
+            </li>
+            <li>
+              <div className={classes.dropdown}>
+                <button className={classes.dropbtn} onClick={DropdownOrder}>
+                  Place Order
+                  <ArrowDropDownIcon
+                    style={{ alignSelf: "center", marginBottom: "-0.4rem" }}
+                  />
+                </button>
+
+                <div
+                  className={
+                    companyDropdown
+                      ? classes.dropdownShow
+                      : classes.dropdownContent
+                  }
+                  id="dropdownCompany"
+                >
+                  <NavLink
+                    to={`/co/order/reguler`}
+                    activeClassName={classes.active}
+                  >
+                    <p>Order Job Slot</p>
+                  </NavLink>
+                  <NavLink
+                    to={`/co/order/candidate`}
+                    activeClassName={classes.active}
+                  >
+                    <p>Order Bulk Candidate</p>
+                  </NavLink>
+                  <NavLink to={`/co/order/es`} activeClassName={classes.active}>
+                    <p>Order Executive Search</p>
+                  </NavLink>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className={classes.dropdown}>
+                <button className={classes.dropbtn} onClick={DropdownList}>
+                  My List
+                  <ArrowDropDownIcon
+                    style={{ alignSelf: "center", marginBottom: "-0.4rem" }}
+                  />
+                </button>
+
+                <div
+                  className={
+                    companyMyList
+                      ? classes.dropdownShow
+                      : classes.dropdownContent
+                  }
+                  id="dropdownCompany"
+                >
+                  <NavLink
+                    to={`/co/${props.auth.userId}/listOrder`}
+                    activeClassName={classes.active}
+                  >
+                    <p>Order List</p>
+                  </NavLink>
+                  <NavLink
+                    to={`/co/${props.auth.userId}/jobList`}
+                    activeClassName={classes.active}
+                  >
+                    <p>Job list</p>
+                  </NavLink>
+                </div>
+              </div>
+            </li>
+          </>
         )}
         {logout}
       </ul>
-      {props.auth.isLoggedIn && props.auth.isCompany && <SideBar />}
       {props.admin.isAdmin && props.admin.isLoggedIn && (
         <div className={classes.active}>
           <SideBar />
