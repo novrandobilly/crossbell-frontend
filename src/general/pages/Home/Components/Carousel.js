b4709be8067fb67ba7b3ec23f5fb619c4db20315
novrandobilly <novrandobilly@gmail.com>
2021-06-03 19:47:15 +0700

    Fix education & financial filter

--- src/general/pages/Home/Components/Carousel.js
+++ src/general/pages/Home/Components/Carousel.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 
 import i1 from '../../../../assets/images/1.jpg';
 import i2 from '../../../../assets/images/2.jpg';
@@ -7,49 +7,40 @@ import i3 from '../../../../assets/images/3.jpg';
 import classes from './Carousel.module.css';
 
 const ImgComp = ({ src }) => {
-  return (
-    <img
-      src={src}
-      alt='slide-img'
-      style={{ width: '100%', height: '100vh', opacity: '0.3' }}
-    />
-  );
+	return <img src={src} alt='slide-img' style={{ width: '100%', height: '100vh', opacity: '0.3' }} />;
 };
 
-const Carousel = (props) => {
-  const backgroundArr = [
-    <ImgComp src={i1} />,
-    <ImgComp src={i2} />,
-    <ImgComp src={i3} />,
-  ];
-  const [x, setX] = useState(0);
+const Carousel = props => {
+	const backgroundArr = useMemo(() => {
+		return [ <ImgComp src={i1} />, <ImgComp src={i2} />, <ImgComp src={i3} /> ];
+	}, []);
+	const [ x, setX ] = useState(0);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      if (x === (backgroundArr.length - 1) * -100) {
-        setX(0);
-      } else {
-        setX(x - 100);
-      }
-    }, 15000);
-    return () => clearInterval(interval);
-  }, [x, backgroundArr]);
+	useEffect(
+		() => {
+			const interval = setInterval(() => {
+				if (x === (backgroundArr.length - 1) * -100) {
+					setX(0);
+				} else {
+					setX(x - 100);
+				}
+			}, 15000);
+			return () => clearInterval(interval);
+		},
+		[ x, backgroundArr ]
+	);
 
-  return (
-    <div className={classes.Slider}>
-      {backgroundArr.map((item, i) => {
-        return (
-          <div
-            key={i}
-            className={classes.Slide}
-            style={{ transform: `translateX(${x}%)` }}
-          >
-            {item}
-          </div>
-        );
-      })}
-    </div>
-  );
+	return (
+		<div className={classes.Slider}>
+			{backgroundArr.map((item, i) => {
+				return (
+					<div key={i} className={classes.Slide} style={{ transform: `translateX(${x}%)` }}>
+						{item}
+					</div>
+				);
+			})}
+		</div>
+	);
 };
 
 export default Carousel;
