87b091e447df30c160465c8214b55beae9c5b9c7
novrandobilly <novrandobilly@gmail.com>
2021-11-08 12:37:51 +0700

    Create Apply Confirmation for job application

--- src/jobs/components/JobCard.js
+++ src/jobs/components/JobCard.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import moment from 'moment';
@@ -15,8 +15,12 @@ import styles from './JobCard.module.scss';
 const JobCard = props => {
   const [jobId, setJobId] = useState(null);
   const [applicantList, setApplicantList] = useState([]);
+  const [applyConfirm, setApplyConfirm] = useState(false);
 
   //=====================================INSTANT APPLY=====================================
+  const onOpenApplyConfirmHandler = () => setApplyConfirm(true);
+  const onCloseApplyConfirmHandler = () => setApplyConfirm(false);
+
   const applyHandler = async () => {
     setJobId(props.jobId);
     const payload = {
@@ -32,32 +36,34 @@ const JobCard = props => {
         setApplicantList(applicantArray);
       }
       setJobId(null);
+      onCloseApplyConfirmHandler();
     } catch (err) {
       console.log(err);
       setJobId(null);
+      onCloseApplyConfirmHandler();
       return props.createJobFail();
     }
+  };
+  const profileCompletionHandler = () => {
+    onCloseApplyConfirmHandler();
+    props.history.push(`/ap/${props.auth.userId}/profile`);
   };
 
   useEffect(() => {
     setApplicantList(props.jobApplicant);
   }, [props.jobApplicant]);
 
-  let instantApplyButton = <Button></Button>;
+  let instantApplyButton;
 
   if (!props.auth.isCompany && props.auth.token) {
     instantApplyButton = (
       <Button
         btnType='InstantApply'
-        onClick={applyHandler}
+        onClick={onOpenApplyConfirmHandler}
         disabled={applicantList.some(appId => appId.toString() === props.auth.userId.toString())}>
         {applicantList.some(appId => appId.toString() === props.auth.userId.toString()) ? 'Applied' : 'Apply'}
       </Button>
     );
-  }
-
-  if (props.job.isLoading && jobId === props.jobId) {
-    instantApplyButton = <LoadingBar />;
   }
 
   //   =====================================================================================
@@ -68,56 +74,86 @@ const JobCard = props => {
   };
 
   return (
-    <div className={styles.JobCard}>
-      <Modal show={props.job.error} onCancel={onCancelHandler}>
-        Tidak dapat melamar pekerjaan untuk saat ini{' '}
-      </Modal>
-      <div className={styles.Logo}>
-        <img src={props.logo && !props.isHidden ? props.logo.url : BlankCompany} alt='company-logo' />
-      </div>
-      <div className={styles.Content}>
-        <div className={styles.ContentHeader}>
-          <div className={styles.TitleDateWrap}>
-            <h3>
-              <Link to={`/jobs/${props.jobId}`} style={{ textDecoration: 'inherit', color: 'inherit' }}>
-                {props.jobTitle}
-              </Link>
-            </h3>
-            <p className={styles.Date}>{moment().diff(moment(props.releasedAt), 'days')} hari lalu</p>
+    <Fragment>
+      <Modal
+        show={applyConfirm}
+        headerText='Anda yakin melamar pekerjaan ini?'
+        onCancel={onCloseApplyConfirmHandler}
+        style={{ top: '30vh', maxWidth: '600px', marginLeft: '-300px', height: '25vh', overflowY: 'auto' }}>
+        <div className={styles.ApplicationModal}>
+          <p>Pastikan profile anda sudah anda lengkapi untuk memudahkan perusahaan memahami potensi diri anda.</p>
+
+          <div className={styles.ApplyButtonContainer}>
+            {props.job.isLoading && jobId === props.jobId ? (
+              <LoadingBar style={{ margin: '0 0 10px' }} />
+            ) : (
+              <>
+                <button type='button' onClick={onCloseApplyConfirmHandler}>
+                  Cancel
+                </button>
+                <button type='button' onClick={applyHandler}>
+                  Apply
+                </button>
+              </>
+            )}
           </div>
-          <Link
-            to={`/jobs/${props.jobId}`}
-            style={{
-              textDecoration: 'inherit',
-              fontWeight: '500',
-              color: 'rgba(0,0,0,0.6)',
-            }}>
-            Details
-          </Link>
-        </div>
-
-        <div className={styles.TopContent}>
-          {props.isHidden ? (
-            <span className={styles.TextSecret}>Perusahaan Dirahasiakan</span>
-          ) : (
-            <Link to={`/co/${props.companyId._id}/profile`} style={{ textDecoration: 'inherit', color: 'inherit' }}>
-              <span className={styles.TextLeft}>{props.company}</span>
-            </Link>
-          )}
-
-          <span className={styles.PlacementLocationProps}>, {props.placementLocation}</span>
-        </div>
-        <div>
-          <em>{props.fieldOfWork?.filter(fow => fow).join(', ')}</em>
-        </div>
-        <div className={styles.BottomContent}>
-          <p className={styles.BottomSalary}>
-            {props.salary && props.salary > 0 ? `IDR ${salary.toLocaleString()} /bulan` : 'Gaji Tidak Ditampilkan'}
+          <p className={styles.ProfileCompletion} onClick={profileCompletionHandler}>
+            <em>Lengkapi profile sekarang</em>
           </p>
         </div>
+      </Modal>
+
+      <div className={styles.JobCard}>
+        <Modal show={props.job.error} onCancel={onCancelHandler}>
+          Tidak dapat melamar pekerjaan untuk saat ini{' '}
+        </Modal>
+        <div className={styles.Logo}>
+          <img src={props.logo && !props.isHidden ? props.logo.url : BlankCompany} alt='company-logo' />
+        </div>
+        <div className={styles.Content}>
+          <div className={styles.ContentHeader}>
+            <div className={styles.TitleDateWrap}>
+              <h3>
+                <Link to={`/jobs/${props.jobId}`} style={{ textDecoration: 'inherit', color: 'inherit' }}>
+                  {props.jobTitle}
+                </Link>
+              </h3>
+              <p className={styles.Date}>{moment().diff(moment(props.releasedAt), 'days')} hari lalu</p>
+            </div>
+            <Link
+              to={`/jobs/${props.jobId}`}
+              style={{
+                textDecoration: 'inherit',
+                fontWeight: '500',
+                color: 'rgba(0,0,0,0.6)',
+              }}>
+              Details
+            </Link>
+          </div>
+
+          <div className={styles.TopContent}>
+            {props.isHidden ? (
+              <span className={styles.TextSecret}>Perusahaan Dirahasiakan</span>
+            ) : (
+              <Link to={`/co/${props.companyId._id}/profile`} style={{ textDecoration: 'inherit', color: 'inherit' }}>
+                <span className={styles.TextLeft}>{props.company}</span>
+              </Link>
+            )}
+
+            <span className={styles.PlacementLocationProps}>, {props.placementLocation}</span>
+          </div>
+          <div>
+            <em>{props.fieldOfWork?.filter(fow => fow).join(', ')}</em>
+          </div>
+          <div className={styles.BottomContent}>
+            <p className={styles.BottomSalary}>
+              {props.salary && props.salary > 0 ? `IDR ${salary.toLocaleString()} /bulan` : 'Gaji Tidak Ditampilkan'}
+            </p>
+          </div>
+        </div>
+        <div className={styles.InstantSubmit}>{instantApplyButton}</div>
       </div>
-      <div className={styles.InstantSubmit}>{instantApplyButton}</div>
-    </div>
+    </Fragment>
   );
 };
 
--- src/shared/UI_Element/Spinner/LoadingBar.js
+++ src/shared/UI_Element/Spinner/LoadingBar.js
@@ -4,7 +4,7 @@ import classes from './LoadingBar.module.css';
 
 const LoadingBar = props => {
   return (
-    <div className={classes.LoadingBar}>
+    <div className={classes.LoadingBar} style={props.style}>
       <div className={classes.Bounce1} style={{ width: props.width || '15px', height: props.width || '15px' }} />
       <div className={classes.Bounce2} style={{ width: props.width || '15px', height: props.width || '15px' }} />
       <div className={classes.Bounce3} style={{ width: props.width || '15px', height: props.width || '15px' }} />
